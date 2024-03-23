d0500c031e7614a01618342a7699355755c054b5
Lozhkina Iuliana <lozhkina_2021@list.ru>
2024-03-23 21:10:32 +0300

    apdated file api.js

--- src/components/api.js
+++ src/components/api.js
@@ -2,87 +2,133 @@ const api = {
   baseUrl: "https://mesto.nomoreparties.co/v1/wff-cohort-8",
   headers: {
     authorization: "90afa859-dbf5-4c4a-9511-58c7fa1eb5ec",
-    "Content-Type": "application/json",
+    "Content-Typeapi": "application/json",
   },
 };
 export const addCard = (name, link) => {
-  return fetch("https://mesto.nomoreparties.co/v1/wff-cohort-8/cards", {
+  return fetch(`${api.baseUrl}/cards`, {
     method: "POST",
-    headers: config.headers,
+    headers: api.headers,
     body: JSON.stringify({
       name,
       link,
     }),
-  }).then((res) => res.json());
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  });
+  
 };
 
 export const getCards = () => {
-  return fetch("https://mesto.nomoreparties.co/v1/wff-cohort-8/cards", {
-    headers: config.headers,
-  }).then((res) => res.json());
+  return fetch(`${api.baseUrl}/cards`, {
+    headers: api.headers,
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  });
 };
 
 export const deleteMyCard = (cardId) => {
   return fetch(
-    `https://mesto.nomoreparties.co/v1/wff-cohort-8/cards/${cardId}`,
+    `${api.baseUrl}/cards/${cardId}`,
     {
       method: "DELETE",
-      headers: config.headers,
+      headers: api.headers,
     }
-  ).then((res) => res.json());
+  ).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  });
 };
 
 export const addLike = (cardId) => {
   return fetch(
-    `https://mesto.nomoreparties.co/v1/wff-cohort-8/cards/likes/${cardId}`,
+    `${api.baseUrl}/cards/likes/${cardId}`,
     {
       method: "PUT",
-      headers: config.headers,
+      headers: api.headers,
     }
-  ).then((res) => res.json());
+  ).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  });
 };
 export const removeLike = (cardId) => {
   return fetch(
-    `https://mesto.nomoreparties.co/v1/wff-cohort-8/cards/likes/${cardId}`,
+    `${api.baseUrl}/cards/likes/${cardId}`,
     {
       method: "DELETE",
-      headers: config.headers,
+      headers: api.headers,
     }
-  ).then((res) => res.json());
+  ).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  });
 };
 export const updateUserInfo = (name, about) => {
-  return fetch("https://mesto.nomoreparties.co/v1/wff-cohort-8/users/me", {
+  return fetch(`${api.baseUrl}/users/me`, {
     method: "PATCH",
-    headers: config.headers,
+    headers: api.headers,
     body: JSON.stringify({
       name,
       about,
     }),
-  }).then((res) => res.json());
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  });
 };
 export const userInfo = () => {
-  return fetch("https://mesto.nomoreparties.co/v1/wff-cohort-8/users/me", {
+  return fetch(`${api.baseUrl}/users/me`, {
     method: "GET",
-    headers: config.headers,
-  }).then((res) => res.json());
+    headers: api.headers,
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  });
 };
 
 export const userAvatar = () => {
-  return fetch("https://mesto.nomoreparties.co/v1/wff-cohort-8/users/me", {
+  return fetch(`${api.baseUrl}/users/me`, {
     method: "GET",
-    headers: config.headers,
-  }).then((res) => res.json());
+    headers: api.headers,
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  });
 };
 
 export const updateUserAvatar = (avatar) => {
   return fetch(
-    "https://mesto.nomoreparties.co/v1/wff-cohort-8/users/me/avatar",
+    `${api.baseUrl}/users/me/avatar`,
     {
       method: "PATCH",
-      headers: config.headers,
+      headers: api.headers,
       body: JSON.stringify({
         avatar,
       }),
     }
-  ).then((res) => res.json());
+  ).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  });;
 };
